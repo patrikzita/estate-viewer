765d9a8f26e7aa7e7bb4e38a896264f220db2fe3
patrikzita <patrikzit@seznam.cz>
2023-07-03 14:37:01 +0200

    Scraping data added to database

---
+++ scraper/scraper.js
@@ -0,0 +1,82 @@
+const puppeteer = require("puppeteer");
+const { Pool } = require("pg");
+
+const url = "https://www.sreality.cz/en/search/for-sale/apartments";
+
+const pool = new Pool({
+  user: "postgres",
+  host: "db",
+  database: "estatedb",
+  password: "password",
+  port: 5432,
+});
+
+const saveToDatabase = async (data) => {
+  for (const item of data) {
+    await pool.query("INSERT INTO apartments (title, imgSrc) VALUES ($1, $2)", [
+      item.title,
+      item.imgSrc,
+    ]);
+  }
+};
+
+const createTable = async () => {
+  const client = await pool.connect();
+  try {
+    await client.query(`
+        CREATE TABLE IF NOT EXISTS apartments (
+          id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          imgSrc TEXT NOT NULL
+        );
+      `);
+    console.log("Table created successfully!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    client.release();
+  }
+};
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--disable-gpu",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+    ],
+  });
+  const page = await browser.newPage();
+
+  await createTable();
+
+  let apartmentsData = [];
+
+  for (let i = 0; i < 2; i++) {
+    await page.goto(url + "?strana=" + (i + 1), { waitUntil: "networkidle2" });
+
+    let data = await page.evaluate((url) => {
+      const apartmentsPods = Array.from(
+        document.querySelectorAll(".property.ng-scope")
+      );
+      const data = apartmentsPods.map((apartment) => ({
+        title: apartment.querySelector(".name").innerText,
+        imgSrc: apartment.querySelector("img").getAttribute("src"),
+      }));
+
+      return data;
+    }, url);
+
+    apartmentsData = apartmentsData.concat(data);
+  }
+
+  console.log(apartmentsData);
+  console.log(apartmentsData.length);
+  await saveToDatabase(apartmentsData);
+
+  await browser.close();
+};
+
+main();
